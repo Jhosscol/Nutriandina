15f24e708a8a3504db6adb849fbd5b78608b8a7f
Yennybel01 <yennybelhv01@gmail.com>
2025-12-10 15:22:31 -0500

    Funcional

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // src/navigation/AppNavigator.js
 import { createStackNavigator } from '@react-navigation/stack';
 import { useProviderAuth } from '../modules/providers/context/ProviderAuthContext';
@@ -176,40 +175,7 @@ function ProviderAuthNavigator() {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="ProviderLogin" component={ProviderLoginScreen} />
       <Stack.Screen name="ProviderRegister" component={ProviderRegisterScreen} />
-=======
-import { createStackNavigator } from '@react-navigation/stack';
-import { useAuth } from '../modules/user-management/context/AuthContext';
-import HealthQuestionnaireScreen from '../modules/user-management/screens/HealthQuestionnaireScreen';
-import LoginScreen from '../modules/user-management/screens/LoginScreen';
-import RegisterScreen from '../modules/user-management/screens/RegisterScreen';
-import SplashScreen from '../screens/SplashScreen';
-import MainTabNavigator from './MainTabNavigator';
 
-const Stack = createStackNavigator();
-
-export default function AppNavigator() {
-  const { user, userProfile, initializing } = useAuth();
-
-  if (initializing) {
-    return <SplashScreen />;
-  }
-
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      {!user ? (
-        <>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Register" component={RegisterScreen} />
-        </>
-      ) : !userProfile?.isProfileComplete ? (
-        <Stack.Screen 
-          name="HealthQuestionnaire" 
-          component={HealthQuestionnaireScreen} 
-        />
-      ) : (
-        <Stack.Screen name="Main" component={MainTabNavigator} />
-      )}
->>>>>>> ab7a124fb3a5517e936ea6d724df028b94f05a51
     </Stack.Navigator>
   );
 }
